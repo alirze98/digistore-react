1b6a1d547e1e5c5f59a1aaef44c349b92a032e4e
ali <alimustang77@gmail.com>
2022-10-11 17:42:20 +0330

    Add pages

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import DraggableCarousel from '../components/DraggableCarousel';
+import Slider from '../components/Slider';
+const Home = () => {
+    return (
+        <div>
+            <Slider />
+            <DraggableCarousel />
+        </div>
+    );
+};
+
+export default Home;
