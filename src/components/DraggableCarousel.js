cf94dbf91664074836c3723c4e1b4bdba79921bf
ali <alimustang77@gmail.com>
2022-10-12 19:24:34 +0330

    Add loader to draggable carousel

--- src/components/DraggableCarousel.js
+++ src/components/DraggableCarousel.js
@@ -3,14 +3,18 @@ import productIcon from "../assets/product-icon.svg";
 import classes from "./DraggableCarousel.module.css";
 import axios from "axios";
 import CarouselItems from "./CarouselItems";
+import loaderGif from "../assets/loader.gif"
 
 const DraggableCarousel = () => {
   const [carouselItems, setCarouselItems] = useState([]);
-  const [errors, setErrors] = useState(null)
+  const [isLoading,setIsLoading] = useState(false);
+  const [errors, setErrors] = useState(null);
 
   useEffect(() => {
+    setIsLoading(true)
      axios.get("https://digistore.glitch.me/products").then(response =>{
          setCarouselItems(response.data);
+         setIsLoading(false)
      }).catch(error =>{
         setErrors(true)
      })
@@ -21,7 +25,8 @@ const DraggableCarousel = () => {
         <img src={productIcon} alt="productIcon" />
         <span>جدیدترین محصولات</span>
       </div>
-      <CarouselItems data={carouselItems} />
+      { isLoading &&<img src={loaderGif} alt="loader" className={classes.loader} /> }
+      {!isLoading &&<CarouselItems data={carouselItems} />}
     </div>
   );
 };
