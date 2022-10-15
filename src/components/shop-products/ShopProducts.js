0f02c119ac61f1e5cd569f59772d520932567e18
ali <alimustang77@gmail.com>
2022-10-15 19:46:04 +0330

    Add loading and error states to shop shop products

--- src/components/shop-products/ShopProducts.js
+++ src/components/shop-products/ShopProducts.js
@@ -5,23 +5,29 @@ import axios from 'axios';
 import ShopProductsList from './ShopProductsList';
 import Footer from '../common/Footer';
 import Filter from '../common/Filter';
+import loaderGif from '../../assets/loader.gif'
 const ShopProducts = () => {
     const [products,setProducts] = useState([]);
     const [isLoading,setIsLoading] = useState(false);
     const [errors,setErrors] = useState(false);
     useEffect(()=>{
+        setIsLoading(true)
         axios.get('https://digistore.glitch.me/products').then(response =>{
             setProducts(response.data)
+            setIsLoading(false)
         }).catch(error =>{
             setErrors(true)
+            setIsLoading(false)
         })
     },[])
     return (
         <div className={classes['shop-products']}>
             <Filter />
-            <Grid container columns={{xl:7,lg:5,md:3,sm:2,xs:1}} rowSpacing={{xl:1}} className={classes.products}>
+           {!isLoading && !errors &&  <Grid container columns={{xl:7,lg:5,md:3,sm:2,xs:1}} rowSpacing={{xl:1}} className={classes.products}>
                 <ShopProductsList data={products} />
-            </Grid>
+            </Grid>}
+            {isLoading && <img src={loaderGif} className={classes.loader} />}
+            {errors && <div className={classes.error}>something went wrong</div>}
             <Footer />
         </div>
     );
